9ebbeca84031bb09a575e3b22ec2bd468faa9701
timurnugaev <decter49pro@gmail.com>
2022-02-15 14:06:38 +0300

    update layout on categories

--- lib/page/categories.dart
+++ lib/page/categories.dart
@@ -2,6 +2,8 @@ import 'dart:developer';
 
 import 'package:chuck_norris_app/api/api.dart';
 import 'package:chuck_norris_app/widget/button.dart';
+import 'package:chuck_norris_app/widget/pageTitleText.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../utils.dart';
@@ -11,27 +13,81 @@ class CategoriesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const String placeholderValue = "Loading categories...";
+    var screenW = MediaQuery.of(context).size.width;
+    var screenH = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
       body: SafeArea(
         child: FutureBuilder(
           future: Api.getCategories(),
-          initialData: const ["Loading..."],
+          initialData: const [placeholderValue],
           builder: (context, snapshot) {
             var data = snapshot.data as List<String>;
-            return ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                String categoryTitle = data[index];
-                return ChuckButton(
-                  categoryTitle,
-                  onPressed: () async {
-                    print("tapped $categoryTitle");
-                    final String joke = await Api.getRandomJoke(category: categoryTitle);
-                    Utils.showJoke(context, joke);
-                  },
-                );
-              },
+            return Stack(
+              children: [
+                Column(
+                  children: [
+                    const SizedBox(height: 24),
+                    Align(
+                      alignment: AlignmentDirectional.topEnd,
+                      child: Image(
+                        image: const AssetImage('./assets/chuckCategories.png'),
+                        width: screenW * 0.6,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: const [
+                    SizedBox(height: 54),
+                    Center(
+                      child: PageTitleText(text: "Select Category"),
+                    ),
+                  ],
+                ),
+                Align(
+                  alignment: AlignmentDirectional.bottomCenter,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        height: screenH * 0.7 ~/ (64+16) * (64+16),
+                        child: ListView.separated(
+                          physics: const BouncingScrollPhysics(),
+                          itemCount: data.length,
+                          itemBuilder: (context, index) {
+                            String categoryTitle = data[index];
+                            return categoryTitle == placeholderValue
+                                ? const Center(child: Text(placeholderValue))
+                                : Column(
+                                    children: [
+                                      SizedBox(
+                                        height: 64,
+                                        child: ChuckButton(
+                                          categoryTitle,
+                                          onPressed: () async {
+                                            print("tapped $categoryTitle");
+                                            final String joke =
+                                                await Api.getRandomJoke(
+                                                    category: categoryTitle);
+                                            Utils.showJoke(context, joke);
+                                          },
+                                        ),
+                                      ),
+                                    ],
+                                  );
+                          },
+                          separatorBuilder: (BuildContext context, int index) {
+                            return const SizedBox(height: 16);
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 42),
+                    ],
+                  ),
+                ),
+              ],
             );
           },
         ),
